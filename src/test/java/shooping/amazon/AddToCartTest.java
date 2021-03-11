9ac16233a937a5fa29765d3b56a9522993d87c99
ishtiaqkhan1990 <ishtiaqkhan1990@gmail.com>
2021-03-11 10:48:19 -0500

    my second commit

---
+++ src/test/java/shooping/amazon/AddToCartTest.java
@@ -0,0 +1,35 @@
+package shooping.amazon;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class AddToCartTest extends Object {
+	@DataProvider
+	public Object[][] readdata()
+	{
+		Object[][] a = new Object[5][2];
+		a[0][0]="shirt";
+		a[0][1]="10";
+		
+		a[1][0]="tshirt";
+		a[1][1]="12";
+		
+		a[2][0]="suit";
+		a[2][1]="50";
+		
+		a[3][0]="pant";
+		a[3][1]="15";
+		
+		a[4][0]="shorts";
+		a[4][1]="20";
+		
+		return a;
+	}
+	@Test(dataProvider="readdata")
+	public void addtocart(String item,String price)
+	{
+		System.out.println(item);
+		System.out.println(price);
+	
+}
+}
